229caeb5210853fe760873081c271113fe9f850f
Theodor Vararu <theo@tvararu.ro>
2014-04-15 20:53:11 +0300

    Collection driven famous surface.

--- client/famono.js
+++ client/famono.js
@@ -1,9 +1,20 @@
 UI.body.rendered = function() {
   var Engine = require('famous/core/Engine');
+  
   var Surface = require('famous/core/Surface');
   var Modifier = require('famous/core/Modifier');
+  var Transform = require('famous/core/Transform');
+  
+  var Transitionable = require("famous/transitions/Transitionable");
+  var SnapTransition = require("famous/transitions/SnapTransition");
+  var SpringTransition = require("famous/transitions/SpringTransition");
+  
+  Transitionable.registerMethod('snap', SnapTransition);
+  Transitionable.registerMethod('spring', SpringTransition);
 
   var mainContext = Engine.createContext();
+
+  mainContext.setPerspective(200);
 
   var containerModifier = new Modifier({
     origin: [0.5, 0.5]
@@ -12,12 +23,27 @@ UI.body.rendered = function() {
   mainContext = mainContext.add(containerModifier);
 
   var square = new Surface({
-    size: [100, 100],
-    content: 'Square!',
+    size: [200, 200],
     properties: {
-      lineHeight: '100px',
+      lineHeight: '200px',
       textAlign: 'center'
     }
+  });
+  
+  Clicks.find().observe({
+    added: function(clickCounter) {
+      square.setContent(clickCounter.number);
+    },
+    
+    changed: function(clickCounter) {
+      square.setContent(clickCounter.number);
+    }
+  });
+
+  square.on('click', function() {
+    var clickCounter = Clicks.findOne();
+
+    Clicks.update(clickCounter._id, { number: clickCounter.number + 1 });
   });
 
   mainContext.add(square);
