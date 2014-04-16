ede8307660e75ef23b509d7f3f76e9a4641c7e07
Theodor Vararu <theo@tvararu.ro>
2014-04-16 21:07:39 +0300

    Simplify master branch.

--- client/famono.js
+++ client/famono.js
@@ -24,26 +24,11 @@ UI.body.rendered = function() {
 
   var square = new Surface({
     size: [200, 200],
+    content: 'Hello.',
     properties: {
       lineHeight: '200px',
       textAlign: 'center'
     }
-  });
-  
-  Clicks.find().observe({
-    added: function(clickCounter) {
-      square.setContent(clickCounter.number);
-    },
-    
-    changed: function(clickCounter) {
-      square.setContent(clickCounter.number);
-    }
-  });
-
-  square.on('click', function() {
-    var clickCounter = Clicks.findOne();
-
-    Clicks.update(clickCounter._id, { number: clickCounter.number + 1 });
   });
 
   mainContext.add(square);
